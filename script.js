333524df36bf668800ac351d46bfd81c9793e211
igotapochahontas <igotapochahontas@yahoo.com>
2022-04-25 12:22:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let bot = new RiveScript();
 
-const message_container = document.querySelector('.messages');
-const form = document.querySelector('form');
-const input_box = document.querySelector('input');
+//const message_container = document.querySelector('wordlistP');
+//const form = document.querySelector('form');
+const input_box = document.querySelector('txt');
 
 //const brains = [
 //  'https://gist.githubusercontent.com/awesammcoder/91e0f6c527bfdc03b8815289ca4af150/raw/6410ce00b7e1ea0dbd28be03b6eaab64252a841d/brain.rive'
@@ -16,18 +16,20 @@ const brains = [
 
 bot.loadFile(brains).then(botReady).catch(botNotReady);
 
-
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  selfReply(input_box.value);
+  input_box.value = '';
+});
 
 function botReply(message){
-  
-  var comp = select('#comparativeP');
-   comp.html(message +" "  +emote);
+  message_container.innerHTML += `<div class="bot">${message}</div>`;
+  location.href = '#edge';
 }
 
 function selfReply(message){
-  
-  var comp = select('#comparativeP');
-   comp.html(message + emote);
+  message_container.innerHTML += `<div class="self">${message}</div>`;
+  location.href = '#edge';
   
   bot.reply("local-user", message).then(function(reply) {
     botReply(reply);
@@ -37,7 +39,7 @@ function selfReply(message){
 function botReady(){
   bot.sortReplies().then(sorted).catch(notSorted) ;
 
-  botReply('Hello World');
+  botReply('Hello');
 }
 
 function botNotReady(err){
