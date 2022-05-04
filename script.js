6a5d9f2a7b2ee6aab296b0d737d7839a551cc6d9
igotapochahontas <igotapochahontas@yahoo.com>
2022-05-04 16:34:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ var input_box = document.querySelector('txt');
 //  'https://gist.githubusercontent.com/awesammcoder/91e0f6c527bfdc03b8815289ca4af150/raw/6410ce00b7e1ea0dbd28be03b6eaab64252a841d/brain.rive'
 //];
 const brains = [
-  'brain.rive'
-'macro.rive'
+//  'brain.rive'
+//'macro.rive'
 //  'dumb.rive'
 ];
 
