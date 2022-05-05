8b8201dd9f3a7d5adc4f0e218c5258e26bbd90c7
igotapochahontas <igotapochahontas@yahoo.com>
2022-05-04 18:28:28 -0700

    Update scripttest.js

--- scripttest.js
+++ scripttest.js
@@ -1,1 +1,27 @@
+let bot = new RiveScript();
+  bot.loadFile('dumb.rive', brainReady, brainError);
+
+  function brainReady() {
+    console.log('Chatbot ready!');
+    bot.sortReplies();
+ //   let num = floor(random(10)) + 1;
+ //   console.log(num);
+    let reply = bot.reply('local-user', 'set ' );
+  }
+
+  function brainError() {
+    console.log('Chatbot error!');
+  }
+
+  let button = select('#submit');
+  let user_input = select('#txt');
+  let output = select('#output');
+
+  button.mousePressed(chat);
+
+  function chat() {
+    let input = user_input.value();
+    let reply = bot.reply('local-user', input);
+    output.html = reply;
+  }
 
